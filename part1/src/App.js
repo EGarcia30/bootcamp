fbafad44fedaa523c74026d18d5fcf2512224d08
Erick Penado <penado188@gmail.com>
2022-07-19 07:21:37 -0600

    final de react(problemas con axios)

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,41 +1,55 @@
 import { Note } from "./Note"
 import { useState, useEffect } from "react"
-import axios from 'axios'
+import { createNote, getAllNotes } from "./services/index"
 
 
 export default function App(props) {
   const [notes, setNotes] = useState([]);
   const [newNotes, setNewNotes] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('')
 
   useEffect(() => {
     console.log("useEffect")
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(json => {
-      setNotes(json);
-    })
-  }, [])
+    setLoading(true)
+    getAllNotes()
+      .then((notes) => {
+        setNotes(notes);
+        setLoading(false)
+      })
+  }, []);
   const handleChange = (e) => {
     setNewNotes(e.target.value)
   }
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    const noteToAddToState = {
-      id: notes.length + 1,
-      title: newNotes,
-      body: new Date().toISOString(),
-    };
 
-    setNotes([...notes, noteToAddToState]);
+    const noteToAddToState = {
+      title: newNotes,
+      body: newNotes,
+      userId: 1
+    };
+    
+    createNote(noteToAddToState)
+      .then(newNotes => {
+        setNotes((prevNotes) => prevNotes.concat(newNotes))
+      })
+      .catch(e => {
+        console.log(e);
+        setError('La API ha petado')
+      })
+
     setNewNotes("");
   }
 
 
   return(
     <div>
+      <h1>Notes</h1>
+      {loading ? 'Cargando..' : ''}
+      {error ? error : ""}
       <ul>
-        <h1>Notes</h1>
         {
           notes
           .map((note) => {
